26715ba0db4914084679906257a9333df2ccc214
Devaxis Engineering <contact@devaxis.fr>
2025-03-11 11:33:07 +0100

    :heavy_plus_sign: (TAILWIND)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,22 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const HomePage = () => {
     return (
-        <View>
-            <Text>HOME PAGE</Text>
+        <View className='flex items-center justify-center h-screen'>
+            <Text className='text-5xl font-bold text-red-500'>HOME PAGE</Text>
 
             <Link href="/pokemon">Page pokemon</Link>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 50,
+        color: "blue",
+    }
+})
 
 export default HomePage;
