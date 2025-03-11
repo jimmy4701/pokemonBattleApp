1794de321dd1ca5fd33886f1e7b6bc641073f5ef
Devaxis Engineering <contact@devaxis.fr>
2025-03-11 10:37:30 +0100

    :lipstick: (TABBAR) Implemented tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,21 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
 const TabsLayout = () => {
     return (
-        <Tabs>
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: "red",
+        }}>
             <Tabs.Screen name='index' options={{
                 headerShown: false,
-                title: "Recherche"
+                title: "Recherche",
+                tabBarIcon: ({color, size}) => <MaterialCommunityIcons name='magnify' color={color} size={size} /> 
             }} />
-            <Tabs.Screen name='pokemon' options={{headerShown: false}} />
+            <Tabs.Screen name='pokemon' options={{
+                headerShown: false
+
+            }} />
         </Tabs>
     )
 }
